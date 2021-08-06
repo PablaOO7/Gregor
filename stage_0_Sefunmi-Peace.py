8d76afd3c2d10e1d6a0099097226c615adb113b4
Sefunmi-Peace <88338315+Sefunmi-Peace@users.noreply.github.com>
2021-08-06 06:39:41 -0700

    Add first solution

---
+++ stage_0_Sefunmi-Peace.py
@@ -0,0 +1,9 @@
+name = 'Sefunmi Peace Akinseye'
+email_address = 'spakunic@gmail.com'
+slack_username = "@Sefunmi-Peace"
+biostack = "Medicinal chemistry and cheminformatics"
+twitter_handle = "@sefunmiPeace1"
+hamming_distance = (6)
+
+# This prints out my personal details
+print(f'Name: {name}\nEmail address: {email_address}\nSlack username: {slack_username}\nBiostack: {biostack}\nTwitter handle: {twitter_handle}\nHamming distance: {hamming_distance}')
