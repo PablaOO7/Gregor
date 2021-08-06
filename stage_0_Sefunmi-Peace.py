0ffe49d996099d38412babb528fa844ca47580c5
Sefunmi-Peace <88338315+Sefunmi-Peace@users.noreply.github.com>
2021-08-06 13:17:45 -0700

    Stage 0 Assignment

--- stage_0_Sefunmi-Peace.py
+++ stage_0_Sefunmi-Peace.py
@@ -2,8 +2,24 @@ name = 'Sefunmi Peace Akinseye'
 email_address = 'spakunic@gmail.com'
 slack_username = "@Sefunmi-Peace"
 biostack = "Medicinal chemistry and cheminformatics"
-twitter_handle = "@sefunmiPeace1"
-hamming_distance = (6)
+twitter_handle = "@SefunmiPeace1"
+
+# Calculating hamming distance between slack_username and twitter_handle
+def hammingdistance(parameter1, parameter2):
+    i = 0
+    cont = 0
+    while (i < len (parameter1)):
+        print(i)
+        if (parameter1[i] != parameter2[i]):
+            cont += 1
+        i += 1
+    return cont
+
+parameter1 = slack_username
+parameter2 = twitter_handle
+
+Hamming_distance = hammingdistance(parameter1, parameter2)
 
 # This prints out my personal details
-print(f'Name: {name}\nEmail address: {email_address}\nSlack username: {slack_username}\nBiostack: {biostack}\nTwitter handle: {twitter_handle}\nHamming distance: {hamming_distance}')
+
+print(f'Name: {name}\nEmail address: {email_address}\nSlack username: {slack_username}\nBiostack: {biostack}\nTwitter handle: {twitter_handle}\nHamming distance: {Hamming_distance}')
