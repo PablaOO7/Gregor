68fc6dab0a8814e7fae1344b4582d02bff3143ae
PablaOO7 <57266535+PablaOO7@users.noreply.github.com>
2021-08-07 16:09:22 +0530

    Update stage_0_pagan.py

--- stage_0_pagan.py
+++ stage_0_pagan.py
@@ -1,19 +1,22 @@
-# algebra
-import pandas as pd
-import numpy as np
+name = "Alexander_Ospina"
+email = "pagano8@tutanota.com"
+slack_user = "@pagano8"
+biostack = "genomics"
+twitter_user = "@pagano8"
 
-def humming (palabra, palabra1):
-    i = 0
-    cont = 0
-    while (i < len(palabra1)):
-        if (palabra[i] != palabra1[i]):
-            cont += 1
-        i += 1
-    return cont
+# Function to calculate Hamming distance
+def hamming(str1, str2):
+	i = 0
+	count = 0
 
+	while(i < len(str1)):
+		if(str1[i] != str2[i]):
+			count += 1
+		i += 1
+	return count
 
-columnas = ['NAME', 'EMAIL', 'SLACK', 'TASK', 'TWITTER', 'HAMMING DISTANCE' ]
-hum= humming('@pagano8','@pagano8')
-datos = ['Alexander_Ospina','pagano8@tutanota.com', '@pagano8', 'genomics', '@pagano8', hum]
-dataframe = pd.DataFrame(datos, index=columnas)
-print(dataframe)
+str1 = slack_user
+str2 = twitter_user
+Hamming_distance = str(hamming(str1, str2))
+ 
+print(f'Name: {name}\nEmail: {email}\nSlack Username: {slack_user}\nBiostack: {biostack}\ntwitter_username: {twitter_user}\nHamming distance: {Hamming_distance}')
