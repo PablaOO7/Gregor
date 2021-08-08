84a01e7c071412b08b0f676dc92ec6199a7d1801
PablaOO7 <57266535+PablaOO7@users.noreply.github.com>
2021-08-09 00:52:53 +0530

    Update Coded.sh

--- Coded.sh
+++ Coded.sh
@@ -1,31 +1,34 @@
-git clone https://github.com/PablaOO7/Gregor.git
+#git clone https://github.com/PablaOO7/Gregor.git
+git clone https://github.com/Fredrick-Kakembo/Gregor.git
 cd Gregor
 echo "receving data... "
+
+if [ -f Gregor.csv ]; then rm Gregor.csv; touch Gregor.csv; else  touch Gregor.csv; fi
 
 python=$(find . -iname "*[^__].py")
 R=$(find . -iname "*.R")
 
-repo="$python \n $R"
-repo=$(echo $repo | tr "\n" "\n")
+#repo="$python \n $R"
+#repo=$(echo $repo | tr "\n" "\n")
  
  
-for file in $repo
+for file in $python $R
 do
     case $file in
    
         *".py")
-            echo -n ".python file is founf"
+            echo  "$file file is found"
             python3 $file >> Gregor.csv
             echo ""
-             echo -n "python data into Gregor.csv"
+             echo "Written python data into Gregor.csv"
             ;;
  
         *".R")
-            echo -n ".R file is found"
-            Rscript $file >> Gregor.csv
-            echo "" >> Gregor.csv
+            echo  "$file file is found"
+            Rscript $file | sed 's/,$//' >> Gregor.csv
+#            echo "" >> Gregor.csv
             echo ""
-            echo -n "R data into Gregor.csv"
+            echo  "Written R data into Gregor.csv"
             ;;
 
         *)
@@ -33,9 +36,8 @@ do
             ;;
     esac
 done
- 
 
-rm *.class compiled_object
+#rm *.class compiled_object
 echo ""
  
 echo "Voila!!!   Check the data in Gregor.csv)"
